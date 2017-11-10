0d39f8fc3c9fffa24d140c714e27216333c3d348
datagutten <datagutten@datagutten.net>
2017-11-10 08:15:06 +0100

    Added function to decode UserAccountControl and added option to force a password change

--- adtools.class.php
+++ adtools.class.php
@@ -224,6 +224,46 @@ class adtools
 		$cn=preg_replace('/(CN=.+?),[A-Z]{2}.+/','$1',$dn);
 		return ldap_rename($this->ad,$dn,$cn,$newparent,true);
 	}
+
+	//https://stackoverflow.com/a/43791392/2630074
+	public function findFlags($flag) {
+
+    $flags    = array();
+    $flaglist = array(
+               1 => 'SCRIPT',
+               2 => 'ACCOUNTDISABLE',
+               8 => 'HOMEDIR_REQUIRED',
+              16 => 'LOCKOUT',
+              32 => 'PASSWD_NOTREQD',
+              64 => 'PASSWD_CANT_CHANGE',
+             128 => 'ENCRYPTED_TEXT_PWD_ALLOWED',
+             256 => 'TEMP_DUPLICATE_ACCOUNT',
+             512 => 'NORMAL_ACCOUNT',
+            2048 => 'INTERDOMAIN_TRUST_ACCOUNT',
+            4096 => 'WORKSTATION_TRUST_ACCOUNT',
+            8192 => 'SERVER_TRUST_ACCOUNT',
+           65536 => 'DONT_EXPIRE_PASSWORD',
+          131072 => 'MNS_LOGON_ACCOUNT',
+          262144 => 'SMARTCARD_REQUIRED',
+          524288 => 'TRUSTED_FOR_DELEGATION',
+         1048576 => 'NOT_DELEGATED',
+         2097152 => 'USE_DES_KEY_ONLY',
+         4194304 => 'DONT_REQ_PREAUTH',
+         8388608 => 'PASSWORD_EXPIRED',
+        16777216 => 'TRUSTED_TO_AUTH_FOR_DELEGATION',
+        67108864 => 'PARTIAL_SECRETS_ACCOUNT'
+    );
+    for ($i=0; $i<=26; $i++){
+        if ($flag & (1 << $i)){
+            array_push($flags, 1 << $i);
+        }
+    }
+    foreach($flags as $k=>&$v) {
+        $v = $v . ' '  . $flaglist[$v];
+    }
+    return $flags;
+}
+
 	
 	//-----------old---------------
 
@@ -276,15 +316,17 @@ class adtools
 	}
 
 	//Reset password for user
-	function change_passord($dn,$password)
+	function change_passord($dn,$password,$must_change_password=false)
 	{
 		if(empty($dn) || empty($password))
 		{
 			$this->error='DN eller passord er ikke opgitt';
 			return false;
 		}
-
-		$result=ldap_mod_replace($this->ad,$dn,array('unicodePwd'=>$this->pwd_encryption($password)));
+		$fields=array('unicodePwd'=>$this->pwd_encryption($password));
+		if($must_change_password!==false)
+			$fields['pwdLastSet']=0;
+		$result=ldap_mod_replace($this->ad,$dn,$fields);
 		if($result===false)
 		{
 			$this->error=ldap_error($this->ad);
