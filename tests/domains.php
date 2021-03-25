b651852786559383fc253349e76f4d708eeaafd6
datagutten <datagutten@datagutten.net>
2021-03-25 08:49:01 +0100

    Add bad configurations for test

--- tests/domains.php
+++ tests/domains.php
@@ -1,4 +1,7 @@
 <?Php
 $domains['test']=array('domain'=>'localhost','dc'=>'localhost' ,'dn'=>'OU=Test,DC=example,DC=com','username'=>'cn=admin,dc=example,dc=com','password'=>'test');
+$domains['missing_dc']=array();
+$domains['no_dc']=array('domain'=>'localhost');
+$domains['no_domain']=array('dc'=>'localhost');
 
 return $domains;
